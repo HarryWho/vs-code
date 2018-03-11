3d9d539a8957d759909562e821908b74d962bd79
HarryWho <37016939+HarryWho@users.noreply.github.com>
2018-03-11 15:51:19 +1100

    Added template

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
-
 const index = require('./routes/index');
-
 const app = express();
+const handlebars = require('express-handlebars').create({defaultLayout:'main'});
 
 app.use('/', index);
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+app.use(express.static(__dirname+'/public'));
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+
 
 app.set('port',process.env.port || 5001);
 
